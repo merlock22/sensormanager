052b99ff28d8f9fa877f5919039129591b5341d7
Eduardo Castano <rails@rails.(none)>
2014-01-03 19:40:19 +0000

    Version 1.1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131103003737) do
+ActiveRecord::Schema.define(version: 20140103192802) do
 
   create_table "categoria_equipos", force: true do |t|
     t.string   "nombre"
@@ -19,5 +19,46 @@ ActiveRecord::Schema.define(version: 20131103003737) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "concentradors", force: true do |t|
+    t.integer  "categoria_equipo_id"
+    t.string   "nombre"
+    t.string   "descripcion"
+    t.string   "estado"
+    t.float    "gis_latitud"
+    t.float    "gis_longitud"
+    t.string   "mac_address"
+    t.string   "numero_serie"
+    t.string   "ip_address"
+    t.integer  "ip_port"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "concentradors", ["categoria_equipo_id"], name: "index_concentradors_on_categoria_equipo_id", using: :btree
+
+  create_table "medidas", force: true do |t|
+    t.integer  "sensor_id"
+    t.datetime "fecha"
+    t.float    "valor"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "medidas", ["sensor_id"], name: "index_medidas_on_sensor_id", using: :btree
+
+  create_table "sensors", force: true do |t|
+    t.integer  "concentrador_id"
+    t.string   "nombre"
+    t.string   "descripcion"
+    t.string   "estado"
+    t.string   "parametro"
+    t.float    "valor"
+    t.string   "uds"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sensors", ["concentrador_id"], name: "index_sensors_on_concentrador_id", using: :btree
 
 end
